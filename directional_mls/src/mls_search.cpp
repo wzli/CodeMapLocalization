706ede2719638843f6b7991631fa615c5d338bb4
WenZheng Li <me@wenzheng.li>
2019-09-16 10:26:27 +0900

    made visit table thread local, added word visited to branch logic

--- directional_mls/src/mls_search.cpp
+++ directional_mls/src/mls_search.cpp
@@ -24,7 +24,7 @@ void MlsSearch::generate_mls(uint8_t n) {
         _least_visited_words.emplace_back(i);
     }
     std::vector<bool> sequence, max_length_sequence;
-    for(int i = 0; i < 5000; ++i) {
+    for(int i = 0; i < 1000; ++i) {
         std::partial_sort(_least_visited_words.begin(), _least_visited_words.begin() + 1,  _least_visited_words.end(),
             [this] (uint32_t a, uint32_t b) {return _words[a].visit_count < _words[b].visit_count;}
         );
@@ -44,30 +44,32 @@ void MlsSearch::generate_mls(uint8_t n) {
 }
 
 void MlsSearch::generate_new_sequence(std::vector<bool>& sequence, uint32_t start_word, uint8_t n) {
+    thread_local std::vector<uint32_t> last_visit_id;
+    thread_local uint16_t visit_id = 0;
     const uint32_t L_BIT = 1 << (n -1);
-    std::vector<bool> visited_words(_words.size());
+    last_visit_id.resize(_words.size(), visit_id);
+    ++visit_id;
     sequence.clear();
     for(uint32_t bit_mask = 1; bit_mask <= L_BIT ; bit_mask <<= 1) {
         sequence.emplace_back(bit_mask & start_word);
     }
-    visited_words[start_word] = true;
+    last_visit_id[start_word] = visit_id;
     ++_words[start_word].visit_count;
     auto extend_sequence = [&] (uint32_t word, bool rev) {
         while(true) {
             word = rev ? (word & ~L_BIT) << 1 : word >> 1;
             uint32_t other_word = word | (rev ? 1 : L_BIT);
-            if(visited_words[reverse_bits(other_word, n)] ) {
-                if(visited_words[reverse_bits(word, n)]) {
+            bool word_visited = last_visit_id[word] == visit_id || last_visit_id[reverse_bits(word, n)] == visit_id;
+            bool other_word_visited = last_visit_id[other_word] == visit_id || last_visit_id[reverse_bits(other_word, n)] == visit_id;
+            if(other_word_visited) {
+                if(word_visited) {
                     break;
                 } 
-            } else if(visited_words[reverse_bits(word, n)] || _words[other_word].visit_count < _words[word].visit_count) {
+            } else if(word_visited || _words[other_word].visit_count < _words[word].visit_count) {
                 word = other_word;
             }
-            if(visited_words[word]) {
-                break;
-            }
             sequence.emplace_back(word & (rev ? 1 : L_BIT));
-            visited_words[word] = true;
+            last_visit_id[word] = visit_id;
             ++_words[word].visit_count;
         }
     };
