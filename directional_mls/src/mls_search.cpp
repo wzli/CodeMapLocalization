910f581c7dc199c82007c16de899aee25ed1280c
WenZheng Li <me@wenzheng.li>
2019-09-16 11:13:10 +0900

    maded visit counts read only in threads

--- directional_mls/src/mls_search.cpp
+++ directional_mls/src/mls_search.cpp
@@ -20,19 +20,23 @@ uint32_t MlsSearch::reverse_bits(uint32_t v, uint8_t n) {
 }
 
 void MlsSearch::generate_mls(uint8_t n) {
-    _words.clear();
+    _word_visit_counts.clear();
     _least_visited_words.clear();
-    _words.resize(1 << n);
-    _least_visited_words.reserve(_words.size());
-    for(uint32_t i = 0; i < _words.size(); ++i) {
+    _word_visit_counts.resize(1 << n);
+    _least_visited_words.reserve(_word_visit_counts.size());
+    for(uint32_t i = 0; i < _word_visit_counts.size(); ++i) {
         _least_visited_words.emplace_back(i);
     }
     std::vector<bool> sequence, max_length_sequence;
-    for(int i = 0; i < 1000; ++i) {
+    std::vector<uint32_t> visited_words;
+    for(int i = 0; i < 2000; ++i) {
         std::partial_sort(_least_visited_words.begin(), _least_visited_words.begin() + 1,  _least_visited_words.end(),
-            [this] (uint32_t a, uint32_t b) {return _words[a].visit_count < _words[b].visit_count;}
+            [this] (uint32_t a, uint32_t b) {return _word_visit_counts[a] < _word_visit_counts[b];}
         );
-        generate_new_sequence(sequence, _least_visited_words.front(), n);
+        generate_new_sequence(sequence, visited_words, _least_visited_words.front(), n);
+        for(uint32_t word : visited_words) {
+            ++_word_visit_counts[word];
+        }
         if(sequence.size() > max_length_sequence.size()) {
             max_length_sequence = sequence;
         }
@@ -41,24 +45,25 @@ void MlsSearch::generate_mls(uint8_t n) {
         std::cout << (int16_t)bit;
     }
     uint32_t optimal_length = 0;
-    for(uint32_t i = 0; i < _words.size(); ++i) {
+    for(uint32_t i = 0; i < _word_visit_counts.size(); ++i) {
         optimal_length += (i <= reverse_bits(i, n));
     }
     std::cout << " size " << max_length_sequence.size() << "/" << optimal_length << " n " << (int16_t)n << std::endl;
 }
 
-void MlsSearch::generate_new_sequence(std::vector<bool>& sequence, uint32_t start_word, uint8_t n) {
+void MlsSearch::generate_new_sequence(std::vector<bool>& sequence, std::vector<uint32_t>& visited_words, uint32_t start_word, uint8_t n) {
     thread_local std::vector<uint32_t> last_visit_id;
     thread_local uint16_t visit_id = 0;
     const uint32_t L_BIT = 1 << (n -1);
-    last_visit_id.resize(_words.size(), visit_id);
+    last_visit_id.resize(_word_visit_counts.size(), visit_id);
     ++visit_id;
     sequence.clear();
+    visited_words.clear();
     for(uint32_t bit_mask = 1; bit_mask <= L_BIT ; bit_mask <<= 1) {
         sequence.emplace_back(bit_mask & start_word);
     }
     last_visit_id[start_word] = visit_id;
-    ++_words[start_word].visit_count;
+    visited_words.emplace_back(start_word);
     auto is_word_visited = [&] (uint32_t word) {
         return last_visit_id[word] == visit_id
                     || last_visit_id[inverse_bits(word, n)] == visit_id
@@ -74,30 +79,15 @@ void MlsSearch::generate_new_sequence(std::vector<bool>& sequence, uint32_t star
                 if(word_visited) {
                     break;
                 } 
-            } else if(word_visited || _words[other_word].visit_count < _words[word].visit_count) {
+            } else if(word_visited || _word_visit_counts[other_word] < _word_visit_counts[word]) {
                 word = other_word;
             }
             sequence.emplace_back(word & (rev ? 1 : L_BIT));
             last_visit_id[word] = visit_id;
-            ++_words[word].visit_count;
+            visited_words.emplace_back(word);
         }
     };
     extend_sequence(start_word, false);
     std::reverse(sequence.begin(), sequence.end());
     extend_sequence(start_word, true);
-    start_word = 0;
-    for(auto bit = sequence.begin(); bit != sequence.begin() + n - 1; ++bit) {
-        start_word = (start_word << 1) ^ *bit;
-    }
-    for(auto bit = sequence.begin() + n - 1; bit != sequence.end(); ++bit) {
-        start_word = ((start_word & ~L_BIT) << 1) ^ *bit;
-        if(_words[start_word].longest_sequence_length < sequence.size()) {
-            _words[start_word].longest_sequence_length = sequence.size();
-        }
-    }
-
-    //for(bool bit : sequence) {
-    //    std::cout << (int16_t)bit;
-    //}
-    //std::cout << " size " << sequence.size() << " n " << (int16_t)n << std::endl;
 }
--- directional_mls/src/mls_search.hpp
+++ directional_mls/src/mls_search.hpp
@@ -4,7 +4,7 @@
 
 class MlsSearch {
   public:
-    void generate_new_sequence(std::vector<bool>& sequence, uint32_t start_word, uint8_t n);
+    void generate_new_sequence(std::vector<bool>& sequence, std::vector<uint32_t>& visited_words, uint32_t start_word, uint8_t n);
     void generate_mls(const uint8_t n);
 
     static uint32_t reverse_bits(uint32_t v, uint8_t n);
@@ -12,11 +12,6 @@ class MlsSearch {
 
   private:
 
-    struct Word {
-        uint32_t longest_sequence_length = 0;
-        uint32_t visit_count = 0;
-    };
-
-    std::vector<Word> _words;
+    std::vector<uint32_t> _word_visit_counts;
     std::vector<uint32_t> _least_visited_words;
 };
