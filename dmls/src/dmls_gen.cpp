6c461b30dba34b733390a79d5e76e5d4e5ed7b07
wenzheng <me@wenzheng.li>
2019-09-23 16:35:20 +0000

    stash

--- dmls/src/dmls_gen.cpp
+++ dmls/src/dmls_gen.cpp
@@ -1,14 +1,15 @@
 #include "dmls_gen.hpp"
 #include <algorithm>
-
 #include <bitset>
+#include <cassert>
 #include <iostream>
 
-uint32_t DmlsGen::inverse_bits(uint32_t v, uint8_t n) {
-  return ~(v | (0xFFFFFFFF << n));
+uint32_t DmlsGen::inverse_bits(uint32_t v) const {
+  return ~(v | (0xFFFFFFFF << _word_len));
 }
 
-uint32_t DmlsGen::reverse_bits(uint32_t v, uint8_t n) {
+uint32_t DmlsGen::reverse_bits(uint32_t v) const {
+  uint8_t n = _word_len;
   v &= ~(0xFFFFFFFF << n);
   unsigned int r = v & 1;
   for (v >>= 1; v; v >>= 1) {
@@ -19,38 +20,86 @@ uint32_t DmlsGen::reverse_bits(uint32_t v, uint8_t n) {
   return r << (n - 1);
 }
 
-void DmlsGen::generate_mls(uint8_t n) {
-  std::vector<bool> start_string{0,1,0};
-  generate_dmls(start_string, n);
+bool DmlsGen::is_word_visited(uint32_t word) const {
+  return _word_visit_table[word] == _visit_id ||
+         _word_visit_table[inverse_bits(word)] == _visit_id ||
+         _word_visit_table[reverse_bits(word)] == _visit_id;
+};
+
+uint32_t DmlsGen::select_new_node(uint32_t word, uint32_t node) const {
+    if(_nodes.child[0]) {
+      if(_nodes.child[1]) {
+      }
+    } else {
+      if(_nodes.child[1]) {
+      } else {
+        assert();
+      }
+    }
+    return 0;
 }
 
-void DmlsGen::generate_dmls(std::vector<bool> &sequence, uint8_t n) {
-  thread_local std::vector<uint32_t> word_visit_table;
-  thread_local uint16_t visit_id = 0;
-  const uint32_t L_BIT = 1 << (n - 1);
-  word_visit_table.resize(1 << n, visit_id);
-  ++visit_id;
+void DmlsGen::generate_mls(uint8_t n) {
+  _word_len = n;
+  std::vector<bool> start_string{0,1,0,1,0,1};
+  std::cout << "error code " << (int)generate_dmls(start_string) << std::endl;
+}
+
+DmlsGen::Error DmlsGen::generate_dmls(std::vector<bool> &sequence) {
+  const uint32_t L_BIT = 1 << (_word_len - 1);
+  // initalize word visit table
+  _word_visit_table.resize(1 << _word_len, _visit_id);
+  ++_visit_id;
   // build start word
   uint32_t word = 0;
-  for (bool bit : sequence) {
-    word = (word >> 1) | (bit << (n - 1));
+  for (uint32_t i = 0; i < sequence.size(); ++i) {
+    word = (word >> 1) | (sequence[i] << (_word_len - 1));
+    // insert words parsed in the start sequence
+    if(i >= _word_len) {
+      if(is_word_visited(word)) {
+        return INVALID_START_SEQUENCE;
+      }
+      _word_visit_table[word] = _visit_id;
+    }
   }
-  if (sequence.size() < n) {
-    word >>= n - sequence.size();
+  if (sequence.size() < _word_len) {
+    word >>= _word_len - sequence.size();
   }
+  // create root node
+  _nodes.clear();
+  _nodes.emplace_back();
+  uint32_t node = 0;
+  // select the least explored node
+
 #if 0
-  word_visit_table[start_word] = visit_id;
-  auto is_word_visited = [&](uint32_t word) {
-    return word_visit_table[word] == visit_id ||
-           word_visit_table[inverse_bits(word, n)] == visit_id ||
-           word_visit_table[reverse_bits(word, n)] == visit_id;
-  };
+  while (true) {
+    word = word >> 1;
+    _nodes[node].child[0] = _nodes.size();
+    _nodes.emplace_back();
+    uint32_t other_word = word | L_BIT;
+    _nodes[node].child[1] = _nodes.size();
+    _nodes.emplace_back();
+    bool word_visited = is_word_visited(word);
+    bool other_word_visited = is_word_visited(other_word);
+    if (other_word_visited) {
+      if (word_visited) {
+        break;
+      }
+    } else if (word_visited ||
+               _word_visit_counts[other_word] < _word_visit_counts[word]) {
+      word = other_word;
+    }
+    sequence.emplace_back(word & (rev ? 1 : L_BIT));
+    _word_visit_table[word] = _visit_id;
+  }
+
+  _word_visit_table[start_word] = _visit_id;
   auto extend_sequence = [&](uint32_t word, bool rev) {
     while (true) {
       word = rev ? (word & ~L_BIT) << 1 : word >> 1;
       uint32_t other_word = word | (rev ? 1 : L_BIT);
-      bool word_visited = is_word_visited(word);
-      bool other_word_visited = is_word_visited(other_word);
+      bool word_visited = is_word_visited(word, n);
+      bool other_word_visited = is_word_visited(other_word, n);
       if (other_word_visited) {
         if (word_visited) {
           break;
@@ -60,7 +109,7 @@ void DmlsGen::generate_dmls(std::vector<bool> &sequence, uint8_t n) {
         word = other_word;
       }
       sequence.emplace_back(word & (rev ? 1 : L_BIT));
-      word_visit_table[word] = visit_id;
+      _word_visit_table[word] = _visit_id;
     }
   };
   for (uint32_t bit_mask = 1; bit_mask <= L_BIT; bit_mask <<= 1) {
@@ -70,4 +119,5 @@ void DmlsGen::generate_dmls(std::vector<bool> &sequence, uint8_t n) {
   std::reverse(sequence.begin(), sequence.end());
   extend_sequence(start_word, true);
 #endif
+  return SUCCESS;
 }
--- dmls/src/dmls_gen.hpp
+++ dmls/src/dmls_gen.hpp
@@ -4,6 +4,11 @@
 
 class DmlsGen {
 public:
+  enum Error {
+    SUCCESS,
+    INVALID_START_SEQUENCE,
+  };
+
   struct Node {
     double explored_rate;
     uint32_t parent;
@@ -12,11 +17,16 @@ public:
 
   void generate_mls(uint8_t n);
 
-  void generate_dmls(std::vector<bool> &sequence, uint8_t n);
+  Error generate_dmls(std::vector<bool> &sequence);
 
 private:
-  static uint32_t inverse_bits(uint32_t v, uint8_t n);
-  static uint32_t reverse_bits(uint32_t v, uint8_t n);
+  uint32_t inverse_bits(uint32_t v) const;
+  uint32_t reverse_bits(uint32_t v) const;
+  bool is_word_visited(uint32_t word) const;
+  uint32_t select_new_node(uint32_t word, uint32_t node) const;
 
   std::vector<Node> _nodes;
+  std::vector<uint32_t> _word_visit_table;
+  uint16_t _visit_id = 0;
+  uint8_t _word_len;
 };
