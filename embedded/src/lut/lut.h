be4fce6788d544fc63fbb2e9640ea18b211e3012
WenZheng Li <me@wenzheng.li>
2019-09-27 03:59:57 +0900

    tested working lut dat generation

--- embedded/src/lut/lut.h
+++ embedded/src/lut/lut.h
@@ -8,6 +8,11 @@ typedef struct {
     uint16_t value : 14;
 } LutEntry;
 
+extern const uint8_t LUT_KEY_LENGTH;
+extern const uint32_t LUT_SIZE;
+extern const uint64_t LUT_UNIQUE_ID;
+extern const LutEntry* LUT_DATA;
+
 void lut_sort(LutEntry* lut, uint16_t len);
 uint16_t lut_search(const LutEntry* lut, uint16_t len, uint32_t key);
 
