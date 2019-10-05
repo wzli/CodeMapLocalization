9912c383b923c56f34e151805970ff6cd5287fb0
WenZheng Li <me@wenzheng.li>
2019-10-06 04:11:25 +0900

    added auto checkpoints

--- autodecoder/autodecoder.py
+++ autodecoder/autodecoder.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import yaml
+import math, yaml, keras
 import numpy as np
-import keras
 import tensorflow as tf
 
 config = tf.ConfigProto()
@@ -21,22 +20,21 @@ word_vectors = []
 position_vectors = []
 
 for position in range(len(sequence) - word_length + 1):
-    word = sequence[position : position + 17]
+    word = sequence[position:position + 17]
     word_position_map[int(word, 2)] = position
     word_vectors.append(np.fromstring(word, dtype=np.int8) - ord('0'))
-    position_vectors.append(np.array([len(sequence), position], dtype = np.int32))
-
+    position_vectors.append(np.array([len(sequence), position],
+                                     dtype=np.int32))
 
 for word in range(2**17):
     if word not in word_position_map:
-        word_string = format(word, '0' + str(word_length) + 'b');
-        word_vectors.append(np.fromstring(word_string, dtype=np.int8) - ord('0'))
-        position_vectors.append(np.array([0, 0], dtype = np.int32))
+        word_string = format(word, '0' + str(word_length) + 'b')
+        word_vectors.append(
+            np.fromstring(word_string, dtype=np.int8) - ord('0'))
+        position_vectors.append(np.array([0, 0], dtype=np.int32))
 
 x_train = np.vstack(word_vectors).astype(np.float32)[..., np.newaxis]
 y_train = np.vstack(position_vectors)
-
-
 ''' this is for binary output encoding
 for position in range(len(sequence) - word_length + 1):
     word_string = sequence[position : position + 17]
@@ -57,24 +55,21 @@ y_train = np.vstack(position_vectors)
 ''' and None
 
 model = keras.models.Sequential([
-    keras.layers.Conv1D(8, 3),
-    keras.layers.Conv1D(8, 3),
-    keras.layers.Conv1D(8, 3),
-    keras.layers.Conv1D(8, 3),
+    keras.layers.Conv1D(8, 3, activation='relu', input_shape=(word_length, 1)),
+    keras.layers.Conv1D(8, 3, activation='relu'),
+    keras.layers.Conv1D(8, 3, activation='relu'),
+    keras.layers.Conv1D(8, 3, activation='relu'),
     keras.layers.Flatten(),
     keras.layers.Dense(2, activation='relu'),
 ])
 
-model.compile(
-    optimizer='adam',
-    loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-
-model.fit(x_train, y_train, epochs=1000)
+save_callback = keras.callbacks.ModelCheckpoint("model_{epoch}_{loss:.0f}.h5",
+                                                period=200)
+model.fit(x_train, y_train, epochs=1000000000, callbacks=[save_callback])
 model.summary()
 model.evaluate(x_train, y_train, verbose=2)
-
-model.save("model.h5")
 
 print(model.predict(x_train[10:20]))
 print(y_train[10:20])
