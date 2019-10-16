73d0f35ca9c9d6bd02144c76e3bfe5c2da250290
WenZheng Li <me@wenzheng.li>
2019-10-16 13:04:36 +0900

    fixed roulette boundary problem

--- dmls_gen/include/roulette.hpp
+++ dmls_gen/include/roulette.hpp
@@ -4,10 +4,11 @@
 template <class WEIGHT_CONTAINER>
 size_t roulette(const WEIGHT_CONTAINER& weights, uint32_t random_number) {
     float total = std::accumulate(weights.begin(), weights.end(), 0.0f);
-    float scaled_num = total * random_number / ~0u;
-    size_t i = 0;
-    for (float threshold = weights[0]; scaled_num > threshold && i < weights.size();
-            threshold += weights[++i])
-        ;
+    const float scaled_num = total * random_number / ~0u;
+    size_t i = weights.size() - 1;
+    ;
+    do {
+        total -= weights[i];
+    } while (scaled_num < total && --i > 0);
     return i;
 }
