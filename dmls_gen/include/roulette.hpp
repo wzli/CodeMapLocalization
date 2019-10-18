e60e11d378671e677dccefc23865661ce6f0e624
WenZheng Li <me@wenzheng.li>
2019-10-18 21:44:23 +0900

    fixed roulette bug

--- dmls_gen/include/roulette.hpp
+++ dmls_gen/include/roulette.hpp
@@ -1,14 +1,21 @@
 #include <cstdint>
 #include <numeric>
+#include <cassert>
 
 template <class WEIGHT_CONTAINER>
 size_t roulette(const WEIGHT_CONTAINER& weights, uint32_t random_number) {
-    float total = std::accumulate(weights.begin(), weights.end(), 0.0f);
-    const float scaled_num = total * random_number / ~0u;
-    size_t i = weights.size() - 1;
-    ;
+    assert(weights.size() > 0);
+    const float total = std::accumulate(weights.begin(), weights.end(), 0.0f);
+    assert(total > 0);
+    float scaled_num = total * random_number / ~0u;
+    size_t i = 0;
+    size_t j = weights.size();
+    while(weights[--j] == 0);
     do {
-        total -= weights[i];
-    } while (scaled_num < total && --i > 0);
+        scaled_num -= weights[i];
+    } while((scaled_num > 0 || weights[i] == 0) && (++i < j));
+    assert(i >= 0);
+    assert(i < weights.size());
+    assert(weights[i] != 0);
     return i;
 }
