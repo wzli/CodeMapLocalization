e5d044cc776f170ea5920e6189e345be9e766a92
WenZheng Li <me@wenzheng.li>
2019-12-06 19:38:31 +0000

    removed stream server from app

--- esp32_cam_app/main/app_httpd.c
+++ esp32_cam_app/main/app_httpd.c
@@ -19,13 +19,7 @@ bool isStreaming = false;
 #endif
 #endif
 
-#define PART_BOUNDARY "123456789000000000000987654321"
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-httpd_handle_t stream_httpd = NULL;
-httpd_handle_t camera_httpd = NULL;
+httpd_handle_t httpd = NULL;
 
 #ifdef CONFIG_LED_ILLUMINATOR_ENABLED
 void enable_led(bool en) {  // Turn LED On or Off
@@ -95,88 +89,6 @@ static esp_err_t capture_handler(httpd_req_t* req) {
     esp_camera_fb_return(fb);
     int64_t fr_end = esp_timer_get_time();
     ESP_LOGI(TAG, "JPG: %uB %ums", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start) / 1000));
-    return res;
-}
-
-static esp_err_t stream_handler(httpd_req_t* req) {
-    camera_fb_t* fb = NULL;
-    esp_err_t res = ESP_OK;
-    size_t _jpg_buf_len = 0;
-    uint8_t* _jpg_buf = NULL;
-    char* part_buf[64];
-
-    static int64_t last_frame = 0;
-    if (!last_frame) {
-        last_frame = esp_timer_get_time();
-    }
-
-    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-    if (res != ESP_OK) {
-        return res;
-    }
-
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-
-#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
-    enable_led(true);
-    isStreaming = true;
-#endif
-
-    while (true) {
-        fb = esp_camera_fb_get();
-        if (!fb) {
-            ESP_LOGE(TAG, "Camera capture failed");
-            res = ESP_FAIL;
-        } else {
-            if (fb->format != PIXFORMAT_JPEG) {
-                bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-                esp_camera_fb_return(fb);
-                fb = NULL;
-                if (!jpeg_converted) {
-                    ESP_LOGE(TAG, "JPEG compression failed");
-                    res = ESP_FAIL;
-                }
-            } else {
-                _jpg_buf_len = fb->len;
-                _jpg_buf = fb->buf;
-            }
-        }
-        if (res == ESP_OK) {
-            size_t hlen = snprintf((char*) part_buf, 64, _STREAM_PART, _jpg_buf_len);
-            res = httpd_resp_send_chunk(req, (const char*) part_buf, hlen);
-        }
-        if (res == ESP_OK) {
-            res = httpd_resp_send_chunk(req, (const char*) _jpg_buf, _jpg_buf_len);
-        }
-        if (res == ESP_OK) {
-            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-        }
-        if (fb) {
-            esp_camera_fb_return(fb);
-            fb = NULL;
-            _jpg_buf = NULL;
-        } else if (_jpg_buf) {
-            free(_jpg_buf);
-            _jpg_buf = NULL;
-        }
-        if (res != ESP_OK) {
-            break;
-        }
-        int64_t fr_end = esp_timer_get_time();
-
-        int64_t frame_time = fr_end - last_frame;
-        last_frame = fr_end;
-        frame_time /= 1000;
-        ESP_LOGI(TAG, "MJPG: %uB %ums (%.1ffps)", (uint32_t)(_jpg_buf_len), (uint32_t) frame_time,
-                1000.0 / (uint32_t) frame_time);
-    }
-
-#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
-    isStreaming = false;
-    enable_led(false);
-#endif
-
-    last_frame = 0;
     return res;
 }
 
@@ -391,38 +303,19 @@ static esp_err_t stats_handler(httpd_req_t* req) {
 void app_httpd_main() {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
-    httpd_uri_t index_uri = {
-            .uri = "/", .method = HTTP_GET, .handler = index_handler, .user_ctx = NULL};
-
     stats_buf = heap_caps_malloc(1024, MALLOC_CAP_SPIRAM);
-    httpd_uri_t stats_uri = {
-            .uri = "/stats", .method = HTTP_GET, .handler = stats_handler, .user_ctx = NULL};
 
-    httpd_uri_t status_uri = {
-            .uri = "/status", .method = HTTP_GET, .handler = status_handler, .user_ctx = NULL};
-
-    httpd_uri_t cmd_uri = {
-            .uri = "/control", .method = HTTP_GET, .handler = cmd_handler, .user_ctx = NULL};
-
-    httpd_uri_t capture_uri = {
-            .uri = "/capture", .method = HTTP_GET, .handler = capture_handler, .user_ctx = NULL};
-
-    httpd_uri_t stream_uri = {
-            .uri = "/stream", .method = HTTP_GET, .handler = stream_handler, .user_ctx = NULL};
+    httpd_uri_t index_uris[] = { 
+        {.uri = "/", .method = HTTP_GET, .handler = index_handler, .user_ctx = NULL},
+        {.uri = "/stats", .method = HTTP_GET, .handler = stats_handler, .user_ctx = NULL},
+        {.uri = "/status", .method = HTTP_GET, .handler = status_handler, .user_ctx = NULL},
+        {.uri = "/control", .method = HTTP_GET, .handler = cmd_handler, .user_ctx = NULL},
+        {.uri = "/capture", .method = HTTP_GET, .handler = capture_handler, .user_ctx = NULL}};
 
     ESP_LOGI(TAG, "Starting web server on port: '%d'", config.server_port);
-    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(camera_httpd, &index_uri);
-        httpd_register_uri_handler(camera_httpd, &cmd_uri);
-        httpd_register_uri_handler(camera_httpd, &status_uri);
-        httpd_register_uri_handler(camera_httpd, &capture_uri);
-        httpd_register_uri_handler(camera_httpd, &stats_uri);
-    }
-
-    config.server_port += 1;
-    config.ctrl_port += 1;
-    ESP_LOGI(TAG, "Starting stream server on port: '%d'", config.server_port);
-    if (httpd_start(&stream_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(stream_httpd, &stream_uri);
+    if (httpd_start(&httpd, &config) == ESP_OK) {
+        for(uint8_t i = 0; i < sizeof(index_uris)/sizeof(httpd_uri_t); ++i) {
+            httpd_register_uri_handler(httpd, index_uris + i);
+        }
     }
 }
