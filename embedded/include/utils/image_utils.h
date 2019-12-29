2cc30ce561793ece3a9fe56e7eb303cbd61b088f
WenZheng Li <me@wenzheng.li>
2019-12-29 23:25:02 +0900

    more img util api changes

--- embedded/include/utils/image_utils.h
+++ embedded/include/utils/image_utils.h
@@ -27,19 +27,11 @@ static const ImageMatrixInt8 sobel_y_kernel = {(int8_t[]){-1, -2, -1, 0, 0, 0, 1
 
 #define PIXEL(MATRIX, ROW, COL) ((MATRIX).data[(ROW) * (MATRIX).n_cols + (COL)])
 
-#define FOR_EACH_PIXEL_PREFIXED_INDEX(MAT, PRE)                     \
-    for (int16_t PRE##row = 0; PRE##row < (MAT).n_rows; ++PRE##row) \
-        for (int16_t PRE##col = 0; PRE##col < (MAT).n_cols; ++PRE##col)
-
-#define FOR_EACH_PIXEL(MAT) FOR_EACH_PIXEL_PREFIXED_INDEX(MAT, )
-
-#define FOR_EACH_PIXEL_IN_WINDOW(WIN)                   \
-    for (int16_t row = (WIN).x0; row < (WIN).x1; ++row) \
-        for (int16_t col = (WIN).y0; col < (WIN).y1; ++col)
+#define FOR_EACH_PIXEL(MAT)                          \
+    for (int16_t row = 0; row < (MAT).n_rows; ++row) \
+        for (int16_t col = 0; col < (MAT).n_cols; ++col)
 
 #define IMG_SIZE(MAT) ((MAT).n_rows * (MAT).n_cols)
-
-#define IMG_SIZE_WINDOW(WIN) (((WIN).x1 - (WIN).x0) * ((WIN).y1 - (WIN).y0))
 
 #define IMG_COPY_SIZE(DST_PTR, SRC)       \
     do {                                  \
@@ -56,16 +48,27 @@ static const ImageMatrixInt8 sobel_y_kernel = {(int8_t[]){-1, -2, -1, 0, 0, 0, 1
 #define IMG_FILL(MAT, VAL) \
     FOR_EACH_PIXEL(MAT) { PIXEL(MAT, row, col) = (VAL); }
 
-#define IMG_APPLY_KERNEL(ACCUMULATOR, KERNEL, MAT, ROW, COL)                                     \
-    FOR_EACH_PIXEL_PREFIXED_INDEX(KERNEL, k_) {                                                  \
-        (ACCUMULATOR) += PIXEL(KERNEL, k_row, k_col) * PIXEL(MAT, k_row + (ROW), k_col + (COL)); \
-    }
-
 #define IMG_SUM(SUM, MAT) \
     FOR_EACH_PIXEL(MAT) { SUM += PIXEL(MAT, row, col); }
 
-#define IMG_SUM_WINDOW(SUM, MAT, WIN) \
-    FOR_EACH_PIXEL_IN_WINDOW(WIN) { SUM += PIXEL(MAT, row, col); }
+#define IMG_MAX(MAX_VAL, MAT) \
+    FOR_EACH_PIXEL(MAT) { MAX_VAL = MAX(MAX_VAL, PIXEL(MAT, row, col)); }
+
+#define IMG_MIN(MIN_VAL, MAT) \
+    FOR_EACH_PIXEL(MAT) { MIN_VAL = MIN(MIN_VAL, PIXEL(MAT, row, col)); }
+
+#define IMG_AVERAGE(AVG, MAT) \
+    do {                      \
+        IMG_SUM(AVG, MAT);    \
+        AVG /= IMG_SIZE(MAT); \
+    } while (0)
+
+#define IMG_APPLY_KERNEL(ACCUMULATOR, KERNEL, MAT, ROW, COL)                                \
+    for (int16_t k_row = 0; k_row < (KERNEL).n_rows; ++k_row)                               \
+        for (int16_t k_col = 0; k_col < (KERNEL).n_cols; ++k_col) {                         \
+            (ACCUMULATOR) +=                                                                \
+                    PIXEL(KERNEL, k_row, k_col) * PIXEL(MAT, k_row + (ROW), k_col + (COL)); \
+        }
 
 #define IMG_THRESHOLD(DST_PTR, SRC, THRESH)                                             \
     do {                                                                                \
@@ -75,30 +78,37 @@ static const ImageMatrixInt8 sobel_y_kernel = {(int8_t[]){-1, -2, -1, 0, 0, 0, 1
         }                                                                               \
     } while (0)
 
-#define IMG_CROP(DST_PTR, SRC, WIN)                                                        \
-    do {                                                                                   \
-        (DST_PTR)->n_cols = (WIN).x1 - (WIN).x0;                                           \
-        (DST_PTR)->n_rows = (WIN).y1 - (WIN).y0;                                           \
-        FOR_EACH_PIXEL_IN_WINDOW(WIN) { PIXEL(*DST_PTR, row, col) = PIXEL(SRC, row, col) } \
+#define IMG_CROP(DST_PTR, SRC, WIN)                                                \
+    do {                                                                           \
+        (DST_PTR)->n_cols = (WIN).x1 - (WIN).x0;                                   \
+        (DST_PTR)->n_rows = (WIN).y1 - (WIN).y0;                                   \
+        FOR_EACH_PIXEL(DST_PTR) {                                                  \
+            PIXEL(*DST_PTR, row, col) = PIXEL(SRC, row + (WIN).y0, col + (WIN).x0) \
+        }                                                                          \
     } while (0)
 
-#define IMG_NORMALIZE(DST_PTR, SRC)                                                            \
-    do {                                                                                       \
-        int32_t max_pixel = PIXEL(SRC, 0, 0);                                                  \
-        int32_t min_pixel = PIXEL(SRC, 0, 0);                                                  \
-        FOR_EACH_PIXEL(SRC) {                                                                  \
-            max_pixel = MAX(max_pixel, PIXEL(SRC, row, col));                                  \
-            min_pixel = MIN(min_pixel, PIXEL(SRC, row, col));                                  \
-        }                                                                                      \
-        IMG_COPY_SIZE(DST_PTR, SRC);                                                           \
-        if (max_pixel == min_pixel) {                                                          \
-            IMG_FILL(*DST_PTR, 0);                                                             \
-        } else {                                                                               \
-            FOR_EACH_PIXEL(SRC) {                                                              \
-                PIXEL(*DST_PTR, row, col) = ((PIXEL(SRC, row, col) - min_pixel) * UINT8_MAX) / \
-                                            (max_pixel - min_pixel);                           \
-            }                                                                                  \
-        }                                                                                      \
+#define IMG_NORMALIZE_RANGE(DST_PTR, SRC, MAX, MIN)                                 \
+    do {                                                                            \
+        assert(MAX > MIN);                                                          \
+        IMG_COPY_SIZE(DST_PTR, SRC);                                                \
+        FOR_EACH_PIXEL(SRC) {                                                       \
+            PIXEL(*DST_PTR, row, col) =                                             \
+                    ((PIXEL(SRC, row, col) - (MIN)) * UINT8_MAX) / ((MAX) - (MIN)); \
+        }                                                                           \
+    } while (0)
+
+#define IMG_NORMALIZE(DST_PTR, SRC)                                  \
+    do {                                                             \
+        int32_t max_pixel = PIXEL(SRC, 0, 0);                        \
+        int32_t min_pixel = PIXEL(SRC, 0, 0);                        \
+        IMG_MAX(max_pixel, SRC);                                     \
+        IMG_MIN(min_pixel, SRC);                                     \
+        if (max_pixel == min_pixel) {                                \
+            IMG_COPY_SIZE(DST_PTR, SRC);                             \
+            IMG_FILL(*DST_PTR, 0);                                   \
+        } else {                                                     \
+            IMG_NORMALIZE_RANGE(DST_PTR, SRC, max_pixel, min_pixel); \
+        }                                                            \
     } while (0)
 
 uint8_t img_bilinear_interpolation(const ImageMatrix mat, Vector2f position);
