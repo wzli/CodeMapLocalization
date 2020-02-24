27b60723ced63844022e43caa14a9be1482ae310
WenZheng Li <me@wenzheng.li>
2020-02-24 09:31:06 +0000

    fixed autoformat script

--- autoformat.sh
+++ autoformat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-find . -type f -name *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
+find . -type f -name '*.h' -o -iname '*.c' -o -iname '*.cpp' -o -iname '*.hpp' \
     | grep -v -e build/ -e esp32-camera/ -e esp-dsp/ \
     | xargs clang-format --verbose -style=file -i -fallback-style=none
 
