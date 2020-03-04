c61a92bfd52d601114c15ce95d945b36af10a23a
WenZheng Li <me@wenzheng.li>
2020-03-04 23:46:48 +0900

    minor localization loop touchup

--- core/src/localization_loop.c
+++ core/src/localization_loop.c
@@ -1,20 +1,6 @@
 #include "localization_loop.h"
 #include <assert.h>
 
-Vector2f img_derotation_filter(
-        ImageMatrix dst, const ImageMatrix src, float rotation_scale, uint8_t bg_fill) {
-    assert(dst.data && src.data);
-    // estimate rotation of original image
-    Vector2f rotation_estimate = img_estimate_rotation(src);
-    if (rotation_estimate.z != 0) {
-        // unrotate
-        rotation_estimate.y *= -rotation_scale;
-        rotation_estimate.x *= rotation_scale;
-        img_rotate(dst, src, rotation_estimate, bg_fill, img_bilinear_interpolation);
-    }
-    return rotation_estimate;
-}
-
 void localization_loop_run(LocalizationContext* ctx, const ImageMatrix image) {
     assert(ctx);
     assert(ctx->sharpened_image.data);
@@ -26,14 +12,12 @@ void localization_loop_run(LocalizationContext* ctx, const ImageMatrix image) {
             ctx->unrotated_image, image, ctx->rotation_scale, ctx->threshold[0]);
     // run optical flow
     img_estimate_translation(&(ctx->correlation), ctx->unrotated_image);
-
     // sharpen unrotated image and remove edge effects
     img_hyper_sharpen(&(ctx->sharpened_image), ctx->unrotated_image);
     ImagePoint image_center = {ctx->sharpened_image.size.x / 2, ctx->sharpened_image.size.y / 2};
-    float complex vertex = 2 * (image_center.x + I * image_center.y) * ctx->rotation_estimate.z;
-    img_draw_regular_polygon(
-            ctx->sharpened_image, image_center, (Vector2f) vertex, 4, ctx->threshold[0], 5);
-
+    Vector2f vertex = {{2 * image_center.x, 2 * image_center.y}};
+    vertex.z *= ctx->rotation_estimate.z;
+    img_draw_regular_polygon(ctx->sharpened_image, image_center, vertex, 4, ctx->threshold[0], 5);
     // find threshold of filtered image
     img_histogram(ctx->histogram, ctx->sharpened_image);
     ctx->histogram[ctx->threshold[0]] = 0;
@@ -51,4 +35,18 @@ void localization_loop_run(LocalizationContext* ctx, const ImageMatrix image) {
     scale_search_location(&(ctx->scale_match), &(ctx->scale_query));
     // compensate rotation estimate
     ctx->scale_match.location.rotation.z *= ctx->rotation_estimate.z;
+}
+
+Vector2f img_derotation_filter(
+        ImageMatrix dst, const ImageMatrix src, float rotation_scale, uint8_t bg_fill) {
+    assert(dst.data && src.data);
+    // estimate rotation of original image
+    Vector2f rotation_estimate = img_estimate_rotation(src);
+    if (rotation_estimate.z != 0) {
+        // unrotate
+        rotation_estimate.y *= -rotation_scale;
+        rotation_estimate.x *= rotation_scale;
+        img_rotate(dst, src, rotation_estimate, bg_fill, img_bilinear_interpolation);
+    }
+    return rotation_estimate;
 }
