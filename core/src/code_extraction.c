64222575cde1acc716b5194143cb746e411396b5
WenZheng Li <me@wenzheng.li>
2020-03-17 02:16:11 +0900

    less hyper sharpen

--- core/src/code_extraction.c
+++ core/src/code_extraction.c
@@ -10,8 +10,8 @@
 void img_hyper_sharpen(ImageMatrix* dst, const ImageMatrix src) {
     IMG_SET_SIZE(*dst, src.size.x - 2, src.size.y - 2);
     FOR_EACH_PIXEL(*dst) {
-        int16_t value = 25 * PIXEL(src, row + 1, col + 1) -
-                        3 * (PIXEL(src, row, col) + PIXEL(src, row, col + 1) +
+        int16_t value = 17 * PIXEL(src, row + 1, col + 1) -
+                        2 * (PIXEL(src, row, col) + PIXEL(src, row, col + 1) +
                                     PIXEL(src, row, col + 2) + PIXEL(src, row + 1, col) +
                                     PIXEL(src, row + 1, col + 2) + PIXEL(src, row + 2, col) +
                                     PIXEL(src, row + 2, col + 1) + PIXEL(src, row + 2, col + 2));
