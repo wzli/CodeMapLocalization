1fb30a6ada6d0ba9addb6389d9cac5803136e1d2
WenZheng Li <me@wenzheng.li>
2020-03-20 19:04:37 +0900

    minor python touchup

--- python/test.py
+++ python/test.py
@@ -21,35 +21,57 @@ def create_window(name, size, pos):
 
 
 class CodeMapGui:
-    max_zoom = 20
     camera_res = 64
+    max_zoom = 20
 
     def __init__(self):
         # initial coordinates
         self.pos = np.array([0, 0])
         self.rotation = 0
         self.zoom = 1
-        self.dragging = False
         # load code map
         self.code_map = cv2.imread('code_map.pbm', cv2.IMREAD_GRAYSCALE)
         # create code map window
         create_window('CodeMap', (512, 512), (0, 0))
         cv2.setMouseCallback('CodeMap', self.code_map_mouse_callback)
-        # create control view
-        create_window('ControlView', (512, 512 + 50), (512, 0))
-        cv2.createTrackbar('Rotation', 'ControlView', 180, 360,
+        # create navigate window
+        create_window('Navigate', (512, 512 + 50), (512, 0))
+        cv2.createTrackbar('Rotation', 'Navigate', 180, 360,
                            self.rotation_callback)
-        cv2.createTrackbar('Zoom', 'ControlView', CodeMapGui.max_zoom,
+        cv2.createTrackbar('Zoom', 'Navigate', CodeMapGui.max_zoom,
                            CodeMapGui.max_zoom * 2, self.zoom_callback)
-        cv2.setMouseCallback('ControlView', self.control_view_mouse_callback)
-        # create camera
+        cv2.setMouseCallback('Navigate', self.navigate_mouse_callback)
+        # create camera window
         create_window('Camera', (256, 256), (1024, 0))
+
+    def update_frame(self):
+        res = CodeMapGui.camera_res
+        # clip position to be within boundary
+        self.pos[0] = np.clip(self.pos[0], 0,
+                              self.code_map.shape[0] - (res // 2) - 1)
+        self.pos[1] = np.clip(self.pos[1], 0,
+                              self.code_map.shape[1] - (res // 2) - 1)
+        # sync rotation track bar
+        cv2.setTrackbarPos('Rotation', 'Navigate', (self.rotation + 180) % 360)
+        # crop navigate view from code map
+        self.navigate = self.code_map[self.pos[1]:self.pos[1] +
+                                      res, self.pos[0]:self.pos[0] + res]
+        # rotate and zoom from navigate view to get camera view
+        rot_mat = cv2.getRotationMatrix2D((0, 0), -self.rotation,
+                                          2 + res / (6 * self.zoom))
+        self.camera = rotate_image(self.navigate, self.rotation, 3 * self.zoom)
+        # draw box around camera view
+        self.navigate = cv2.cvtColor(self.navigate, cv2.COLOR_GRAY2RGB)
+        corners = np.array([[-1, -1], [-1, 1], [1, 1], [1, -1]])
+        for i, corner in enumerate(corners):
+            corners[i] = np.int0(rot_mat[:, :2].dot(corner)) + (res // 2)
+        cv2.drawContours(self.navigate, [corners], 0, (0, 255, 0), 2)
 
     def run(self):
         self.update_frame()
         cv2.imshow('CodeMap', self.code_map)
         while (self.key_callback(cv2.waitKey(50))):
-            cv2.imshow('ControlView', self.control_view)
+            cv2.imshow('Navigate', self.navigate)
             cv2.imshow('Camera', self.camera)
         cv2.destroyAllWindows()
 
@@ -76,20 +98,12 @@ class CodeMapGui:
         self.update_frame()
         return True
 
-    def code_map_mouse_callback(self, event, x, y, flags, param):
-        if flags == cv2.EVENT_FLAG_RBUTTON:
-            border = CodeMapGui.camera_res // 2
-            self.pos[0] = x - border
-            self.pos[1] = y - border
-            self.update_frame()
-
-    def control_view_mouse_callback(self, event, x, y, flags, param):
+    def navigate_mouse_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
-            self.dragging = True
             self.prev_mouse_pos = np.array((x, y))
         elif event == cv2.EVENT_LBUTTONUP or event == cv2.EVENT_RBUTTONUP:
-            self.dragging = False
-        elif event == cv2.EVENT_MOUSEMOVE and self.dragging:
+            del self.prev_mouse_pos
+        elif event == cv2.EVENT_MOUSEMOVE and hasattr(self, 'prev_mouse_pos'):
             mouse_pos = np.array((x, y))
             displacement = mouse_pos - self.prev_mouse_pos
             self.prev_mouse_pos = mouse_pos
@@ -98,6 +112,13 @@ class CodeMapGui:
             elif flags == cv2.EVENT_FLAG_RBUTTON:
                 self.rotation += 2 * displacement[0]
                 self.increment_zoom(-displacement[1])
+            self.update_frame()
+
+    def code_map_mouse_callback(self, event, x, y, flags, param):
+        if flags == cv2.EVENT_FLAG_RBUTTON:
+            border = CodeMapGui.camera_res // 2
+            self.pos[0] = x - border
+            self.pos[1] = y - border
             self.update_frame()
 
     def rotation_callback(self, rotation):
@@ -109,34 +130,9 @@ class CodeMapGui:
         self.update_frame()
 
     def increment_zoom(self, inc):
-        zoom_track = cv2.getTrackbarPos('Zoom', 'ControlView')
+        zoom_track = cv2.getTrackbarPos('Zoom', 'Navigate')
         zoom_track = np.clip(zoom_track + inc, 0, 2 * CodeMapGui.max_zoom)
-        cv2.setTrackbarPos('Zoom', 'ControlView', zoom_track)
-
-    def update_frame(self):
-        res = CodeMapGui.camera_res
-        # clip position to be within boundary
-        self.pos[0] = np.clip(self.pos[0], 0,
-                              self.code_map.shape[0] - (res // 2) - 1)
-        self.pos[1] = np.clip(self.pos[1], 0,
-                              self.code_map.shape[1] - (res // 2) - 1)
-        # sync rotation track bar
-        cv2.setTrackbarPos('Rotation', 'ControlView',
-                           (self.rotation + 180) % 360)
-        # crop control view from code map
-        self.control_view = self.code_map[self.pos[1]:self.pos[1] +
-                                          res, self.pos[0]:self.pos[0] + res]
-        # rotate and zoom from control view to get camera
-        rot_mat = cv2.getRotationMatrix2D((0, 0), -self.rotation,
-                                          2 + res / (6 * self.zoom))
-        self.camera = rotate_image(self.control_view, self.rotation,
-                                   3 * self.zoom)
-        # draw box around camera view
-        self.control_view = cv2.cvtColor(self.control_view, cv2.COLOR_GRAY2RGB)
-        corners = np.array([[-1, -1], [-1, 1], [1, 1], [1, -1]])
-        for i, corner in enumerate(corners):
-            corners[i] = np.int0(rot_mat[:, :2].dot(corner)) + (res // 2)
-        cv2.drawContours(self.control_view, [corners], 0, (0, 255, 0), 2)
+        cv2.setTrackbarPos('Zoom', 'Navigate', zoom_track)
 
 
 # create control window
