b4d55b06dc091a4a38d45daaa157e43f74bd1dd9
WenZheng Li <me@wenzheng.li>
2020-03-28 17:55:09 +0900

    minor refactor in position decode

--- core/include/impl/position_decode_impl.h
+++ core/include/impl/position_decode_impl.h
@@ -53,26 +53,28 @@ AxisPosition TEMPLATE(ac, WIDTH, _decode_position)(AxisCode axiscode) {
         }
         position.span = position.center != MLSQ_NOT_FOUND;
         if (position.span && valid_segment_length > MLS_INDEX.code_length) {
-            TEMPLATE(uint, WIDTH, _t)
-            extended_code = position.inverted
-                                    ? invert_bits(*bits, valid_segment_length)
-                                    : *bits & TEMPLATE(mask_bits_, WIDTH, )(valid_segment_length);
-            int32_t lookup_index = position.reversed ? position.center + MLS_INDEX.code_length -
-                                                               valid_segment_length
-                                                     : position.center;
+            int32_t lookup_index = position.center;
+            if (position.reversed) {
+                lookup_index += MLS_INDEX.code_length - valid_segment_length;
+            }
             if (lookup_index < 0) {
                 valid_segment_length += lookup_index;
                 lookup_index = 0;
-            } else if (lookup_index + valid_segment_length >= MLS_INDEX.sequence_length) {
+            }
+            if (lookup_index + valid_segment_length >= MLS_INDEX.sequence_length) {
                 valid_segment_length = MLS_INDEX.sequence_length - lookup_index - 1;
             }
             TEMPLATE(uint, WIDTH, _t)
             expected_code = TEMPLATE(bv32_get_slice_, WIDTH, )(
                     MLS_INDEX.sequence, lookup_index, valid_segment_length);
+            TEMPLATE(uint, WIDTH, _t)
+            extended_code = position.inverted
+                                    ? invert_bits(*bits, valid_segment_length)
+                                    : *bits & TEMPLATE(mask_bits_, WIDTH, )(valid_segment_length);
             if (position.reversed) {
                 expected_code = reverse_bits(expected_code, valid_segment_length);
             }
-            uint8_t first_diff = count_trailing_zeros(extended_code ^ expected_code);
+            uint8_t first_diff = count_trailing_zeros(expected_code ^ extended_code);
             position.span += MIN(first_diff, valid_segment_length) - MLS_INDEX.code_length;
         }
         if (position.span > best_position.span) {
