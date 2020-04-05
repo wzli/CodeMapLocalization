eee5697f9fd820dc2c5382bdcb939e281fae018b
WenZheng Li <me@wenzheng.li>
2020-04-05 19:04:07 +0900

    added template ros2 package

---
+++ ros2/code_map_localization/code_map_localization/serial.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from code_map_localization.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2/code_map_localization/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'code_map_localization'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name, 'codemap'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wzli',
+    maintainer_email='me@wenzheng.li',
+    description='ROS2 wrapper camera based code map localization',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['serial = code_map_localization.serial:main'],
+    },
+)
