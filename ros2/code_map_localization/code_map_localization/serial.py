742b0cea557ed2d65c639c22972d554bb33b977f
WenZheng Li <me@wenzheng.li>
2020-04-06 19:07:09 +0900

    added ros publishing of localization message

--- ros2/code_map_localization/code_map_localization/serial.py
+++ ros2/code_map_localization/code_map_localization/serial.py
@@ -1,11 +1,20 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
+from code_map_localization_msgs.msg import Localization
 from codemap.serial import CsvRxStream
 import math
 
 
-def location_to_pose(loc_msg):
+def copy_attributes(obj, dic):
+    for key, value in dic.items():
+        if isinstance(value, dict):
+            copy_attributes(getattr(obj, key), value)
+        else:
+            setattr(obj, key, type(getattr(obj, key))(value))
+
+
+def convert_to_ros_msgs(loc_msg):
     pose_msg = PoseStamped()
     pose_msg.header.stamp.sec = loc_msg['timestamp'] >> 10
     pose_msg.header.stamp.nanosec = (loc_msg['timestamp'] & 0x3FF) << 20
@@ -15,7 +24,9 @@ def location_to_pose(loc_msg):
                                            loc_msg['odometry']['rotation'])
     pose_msg.pose.orientation.z = math.sin(0.5 *
                                            loc_msg['odometry']['rotation'])
-    return pose_msg
+    localization_msg = Localization()
+    copy_attributes(localization_msg, loc_msg)
+    return pose_msg, localization_msg
 
 
 class CodeMapLocalization(Node):
@@ -23,6 +34,8 @@ class CodeMapLocalization(Node):
         super().__init__('code_map_localization')
         # create publisher
         self.pose_publisher = self.create_publisher(PoseStamped, 'pose', 1)
+        self.localization_publisher = self.create_publisher(
+            Localization, 'localization', 1)
         # parse serial device
         serial_device = self.declare_parameter("serial_device")
         serial_device = serial_device._value if serial_device else '/dev/ttyUSB0'
@@ -44,9 +57,10 @@ class CodeMapLocalization(Node):
     def rx_timer_callback(self):
         loc_msg = self.rx_stream.read_message()
         if loc_msg is not None:
-            pose_msg = location_to_pose(loc_msg)
+            pose_msg, localization_msg = convert_to_ros_msgs(loc_msg)
             pose_msg.header.frame_id = self.frame_id
             self.pose_publisher.publish(pose_msg)
+            self.localization_publisher.publish(localization_msg)
 
 
 def main(args=None):
