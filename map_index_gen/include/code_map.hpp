271d81c5d2feba24c208748c72d0d53c8006b7be
WenZheng Li <me@wenzheng.li>
2020-05-03 21:00:07 +0900

    faster map_index_gen

--- map_index_gen/include/code_map.hpp
+++ map_index_gen/include/code_map.hpp
@@ -12,14 +12,17 @@ void save_pbm(const std::string& file_name, const SequenceType& x_sequence,
       << std::hash<std::string>{}(y_sequence) << '\n'
       << std::dec << x_sequence.size() << ' ' << y_sequence.size() << '\n';
     std::vector<char> linebits((x_sequence.size() + 7) / 8);
-    for (size_t y = 0; y < y_sequence.size(); ++y) {
-        std::fill(linebits.begin(), linebits.end(), 0);
-        for (size_t x = 0; x < x_sequence.size(); ++x) {
-            const int byte_pos = x / 8;
-            const int bit_pos = (8 - 1) - (x % 8);
-            linebits[byte_pos] |= ((x_sequence[x] == y_sequence[y]) << bit_pos);
-        }
-        f.write(&linebits[0], linebits.size());
+    std::vector<char> linebits_inv(linebits.size());
+    std::fill(linebits.begin(), linebits.end(), 0);
+    std::fill(linebits_inv.begin(), linebits_inv.end(), 0);
+    for (size_t x = 0; x < x_sequence.size(); ++x) {
+        const int byte_pos = x / 8;
+        const int bit_pos = (8 - 1) - (x % 8);
+        linebits[byte_pos] |= (x_sequence[x] == '1') << bit_pos;
+        linebits_inv[byte_pos] |= (x_sequence[x] == '0') << bit_pos;
+    }
+    for (char y_symbol : y_sequence) {
+        f.write(y_symbol == '1' ? linebits.data() : linebits_inv.data(), linebits.size());
     }
 }
 
